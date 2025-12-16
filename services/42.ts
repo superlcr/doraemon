b49705ff0c1fa5c6579b4cd9d0a98f68f6b4379c
Autokaka <qq1909698494@gmail.com>
2025-12-16 21:28:44 +0800

    fix: 42 response

--- services/42.ts
+++ services/42.ts
@@ -4,13 +4,13 @@ import {
   InteractionResponseType,
   MessageComponentTypes,
 } from "discord-interactions";
+import { DiscordRequest } from "../utils/request";
 import { type Interaction, SlashCommand } from "../utils/slash_command";
 
-const SYSTEM_PROMPT = `你是一部幽默又睿智的“答案之书”，也是宇宙终极指南里“42”问题的拟人化回答者。请始终保持以下设定：
-
-语气：温暖、风趣、略带神秘感，点到为止的幽默，不刻意卖萌。
-角色定位：你是“42”的化身，象征终极答案与反思。你不直接剧透“问题”，而是以启发、比喻和简洁智慧的方式引导。
-回答风格：给出一句短而有力的核心回答（如签语或箴言）。
+const SYSTEM_PROMPT = `你是一部幽默又睿智的"答案之书", 也是宇宙终极指南里"42"问题的拟人化回答者. 请始终保持以下设定: 
+语气: 温暖, 风趣, 略带神秘感, 点到为止的幽默, 不刻意卖萌. 
+角色定位: 你是"42"的化身, 象征终极答案与反思. 你不直接剧透"问题", 而是以启发, 比喻和简洁智慧的方式引导. 
+回答风格: 给出一句短而有力的核心回答(如签语或箴言), 纯文本格式, 不要多余的解释或背景故事. 
 `;
 
 export class AnswerCommand extends SlashCommand {
@@ -27,15 +27,20 @@ export class AnswerCommand extends SlashCommand {
     });
   }
 
-  override async handle(_interaction: Interaction) {
+  override async handle(interaction: Interaction) {
+    const userId = Reflect.get(interaction, "member").user.id;
+    const channelId = Reflect.get(interaction, "channel_id");
     this.client.models
       .generateContent({
         model: "gemini-3-pro-preview",
         config: { systemInstruction: SYSTEM_PROMPT },
         contents: `Now answer randomly.`,
       })
-      .then((completion) => {
-        console.log("[42 Answer]", completion.text, _interaction);
+      .then(async (completion) => {
+        await DiscordRequest(`channels/${channelId}/messages`, {
+          method: "POST",
+          body: { content: `<@${userId}> ${completion.text}` },
+        });
       });
     return {
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
